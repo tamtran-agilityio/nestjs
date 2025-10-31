46f57ccfa8b6aaf516a1df03c0f6b592d0c2bb6a
Tam Tran <tam.tran@asnet.com.vn>
2025-10-31 14:57:21 +0700

    Handle decorators

--- src/common/decorators/auth.decorator.ts
+++ src/common/decorators/auth.decorator.ts
@@ -1,11 +1,14 @@
 import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
-import { Roles } from './roles.decorator';
 import { RolesGuard } from '../guards/roles.guard';
 import { AuthGuard } from '../guards/auth.guard';
 
-export function Auth(...roles: string[]) {
+/**
+ * Custom decorator to apply authentication and role-based authorization
+ * @param roles string[]
+ */
+export const Auth = (...roles: string[]) => {
   return applyDecorators(
     SetMetadata('roles', roles),
     UseGuards(AuthGuard, RolesGuard),
   );
-}
+};
